bd0d092f1526d3582d0f2ed0bf07ec196f56b839
jason parra gomez <yayr8@hotmail.com>
2020-03-25 17:52:01 -0400

    .

--- src/components/dashboard/dashboardList.js
+++ src/components/dashboard/dashboardList.js
@@ -1,51 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ListItem } from 'react-native-elements'
-import {getRequest} from "../../api/api"
+import { getRequest } from "../../api/api"
 import axios from "axios"
 
-// const list = [
-//   {
-//     name: 'Amy Farha',
-//     avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-//     subtitle: 'Vice President'
-//   },
-//   {
-//     name: 'Chris Jackson',
-//     avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-//     subtitle: 'Vice Chairman'
-//   }
-// ]
 
 export default class DashboardList extends React.Component {
-  
-  constructor(){
+
+  constructor() {
     super();
-    this.state={
-      data:{}
+    this.state = {
+      data: {}
     }
   }
 
 
-  componentDidMount= async ()=>{
+  componentDidMount = async () => {
     await this.getData()
   }
 
-  getData = ()=>{
+  getData = () => {
     axios.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR&api_key={e49f0c3d16d1d870dd7935b2964b0439059d9bd1601ecb1c64950a102f04ce6b}")
-    .then(response=>{
-      console.log(response.data)
-      this.setState({
-        data: response.data
+      .then(response => {
+        console.log(response.data)
+        this.setState({
+          data: response.data
+        })
       })
-    })
   }
 
   render() {
     return (
       <View>
         <Text>{JSON.stringify(this.state.data)}</Text>
-         {/* {
+        {/* {
           list.map((l, i) => (
             <ListItem
               key={i}
